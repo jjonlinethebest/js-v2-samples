20ac0e5b8e66d6f268f78cb3694331880cbf40c8
jjonlinethebest <31420811+jjonlinethebest@users.noreply.github.com>
2017-08-28 22:39:38 +0200

    search.xml

--- geocoder/python/SimpleParser.py
+++ geocoder/python/SimpleParser.py
@@ -1,17 +1,19 @@
-import simplejson, urllib
-
-GEOCODE_BASE_URL = 'http://maps.google.com/maps/api/geocode/json'
-
-def geocode(address="New+York,+NY",sensor="false", **geo_args):
-    geo_args.update({
-        'address': address,
-        'sensor': sensor  
-    })
-
-    url = GEOCODE_BASE_URL + '?' + urllib.urlencode(geo_args)
-    result = simplejson.load(urllib.urlopen(url))
-       
-    print simplejson.dumps([s['formatted_address'] for s in result['results']], indent=2)
-
-if __name__ == '__main__':
-    geocode(address="San+Francisco",sensor="false")
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+   <element>
+      <faces>1</faces>
+      <locations>
+         <element>
+            <h>119</h>
+            <w>119</w>
+            <x>33</x>
+            <y>23</y>
+         </element>
+      </locations>
+      <name>Scan%205.jpeg</name>
+   </element>
+   <element>
+      <faces>0</faces>
+      <name>Scan%204.jpeg</name>
+   </element>
+</root>
